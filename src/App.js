14c504abab2972ed8ceb87bc1f9652b27290c53d
LvnReys16 <zethkennethpungtod@gmail.com>
2021-03-26 10:46:15 +0800

    the second deployment test run

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn the Second Deployment test run
         </a>
       </header>
     </div>
