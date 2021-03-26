fb3f4b80714c6d829c69e2807806a90c660480eb
LvnReys16 <zethkennethpungtod@gmail.com>
2021-03-26 09:57:49 +0800

    dployment test run

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          THis is the Deployment Test Run
         </p>
         <a
           className="App-link"
